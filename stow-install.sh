9eff99844f8b45b5713ccf0b1193d26a622740b4
imsofi <imsofi@users.noreply.github.com>
2020-06-11 02:10:54 +0200

    update

--- stow-install.sh
+++ stow-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 stow -vSt ~ bash &&
-stow -vSt ~ npm &&
 stow -vSt ~ scripts &&
+stow -vSt ~ vim &&
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim &&
+echo "Write ':PluginInstall' into vim to complete install"
