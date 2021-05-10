04cbc0b6e7f260ecc90234450fde93ca28b4575b
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2021-05-10 16:55:43 +0200

    Running nightly builds have bitten my ass commit

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -35,7 +35,9 @@ require('packer').startup(function()
     -- Collection of configurations for the built-in LSP client
     use 'neovim/nvim-lspconfig'
     -- Easy install of language servers
-    use 'kabouzeid/nvim-lspinstall'
+    -- use 'kabouzeid/nvim-lspinstall'
+    -- Auto completion plugin
+    use 'hrsh7th/nvim-compe'
     -- Treesitter highlighting
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     -- Show the colors for color codes
@@ -93,9 +95,8 @@ require 'colorizer'.setup {
     }
 }
 
-
 -- LSPInstall
-require'lspinstall'.setup()
+-- require'lspinstall'.setup()
 
 -- LSP
 local nvim_lsp = require('lspconfig')
@@ -120,13 +121,103 @@ local on_attach = function(_client, bufnr)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 end
+nvim_lsp.pyright.setup{
+    cmd = { ".npm-global/bin/pyright-langserver", "--stdio" },
+    filetypes = { "python" },
+    on_attach = on_attach,
+    settings = {
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                diagnosticSeverityOverrides = {
+                    reportUnusedImport = "warning"
+                }
+            }
+        }
+    }
+}
 -- -- Enable language servers
-local servers = require'lspinstall'.installed_servers()
-for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup { on_attach = on_attach }
-end
+-- local servers = require'lspinstall'.installed_servers()
+-- for _, lsp in ipairs(servers) do
+--     config = { on_attach = on_attach }
+--     nvim_lsp[lsp].setup(config)
+-- end
 -- -- Map :Format to vim.lsp.buf.formatting()
 vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
+
+-- Compe
+vim.o.completeopt = "menuone,noselect"
+require'compe'.setup {
+    enabled = true;
+    autocomplete = false;
+    debug = false;
+    min_length = 1;
+    preselect = false;
+    throttle_time = 80;
+    source_timeout = 200;
+    incomplete_delay = 400;
+    max_abbr_width = 100;
+    max_kind_width = 100;
+    max_menu_width = 100;
+    documentation = true;
+  
+    source = {
+        path = true;
+        buffer = true;
+        calc = true;
+        nvim_lsp = true;
+        nvim_lua = true;
+        vsnip = false;
+        ultisnips = false;
+    };
+}
+-- -- Set up default shortcuts
+    local opts = { silent=true, expr=true }
+    vim.api.nvim_buf_set_keymap(bufnr, 'i', '<C-CR>', 'compe#complete()', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'i', '<CR>', 'compe#confirm("<CR>")', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'i', '<C-e>', 'compe#close("<C-e>")', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'i', '<C-f>', 'compe#scroll({ "delta": +4 })', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'i', '<C-d>', 'compe#scroll({ "delta": -4 })', opts)
+
+-- -- Navigate completion menu
+    local t = function(str)
+      return vim.api.nvim_replace_termcodes(str, true, true, true)
+    end
+
+    local check_back_space = function()
+        local col = vim.fn.col('.') - 1
+        if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+            return true
+        else
+            return false
+        end
+    end
+ 
+    -- Use (s-)tab to:
+    --- move to prev/next item in completion menuone
+    --- jump to prev/next snippet's placeholder
+    _G.tab_complete = function()
+      if vim.fn.pumvisible() == 1 then
+        return t "<C-n>"
+      elseif check_back_space() then
+        return t "<Tab>"
+      else
+        return vim.fn['compe#complete']()
+      end
+    end
+    _G.s_tab_complete = function()
+      if vim.fn.pumvisible() == 1 then
+        return t "<C-p>"
+      else
+        return t "<S-Tab>"
+      end
+    end
+
+    vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+    vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+    vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+    vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
 
 -- Configuration
@@ -183,6 +274,8 @@ vim.bo.shiftwidth = 4         -- Number of spaces to indicate a tab TODO
 vim.bo.tabstop = 4            -- Number of spaces to insert for tab TODO
 vim.bo.smartindent = true     -- Make indenting smart
 
+-- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
+vim.wo.colorcolumn = "99999"
 
 -- Extras
 -- -- Show hidden characters
