e7f9b326a1ef394379219ce867d0675a563294ef
Sofi <sofi+git@mailbox.org>
2022-03-16 19:49:47 +0100

    Work around treesitter issue in indent line

--- configs/nvim/.config/nvim/lua/config/indentline.lua
+++ configs/nvim/.config/nvim/lua/config/indentline.lua
@@ -2,6 +2,7 @@ require("indent_blankline").setup {
   buftype_exclude = { "terminal", "man", "help", "nofile" },
   filetype_exclude = { "man", "help" },
   bufname_exclude = { "man", "help" },
+  show_first_indent_level = false,
   show_trailing_blankline_indent = false,
-  use_treesitter = "v:true",
+  -- use_treesitter = true, -- Currently broken
 }
