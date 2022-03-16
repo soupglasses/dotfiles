4c9116f051091eb61db1aa71af5402ff2511b0a4
Sofi <sofi+git@mailbox.org>
2022-03-16 21:28:03 +0100

    Small changes to nvim-cmp

--- configs/nvim/.config/nvim/lua/plugins/lsp/completion.lua
+++ configs/nvim/.config/nvim/lua/plugins/lsp/completion.lua
@@ -10,17 +10,17 @@ cmp.setup({
     end,
   },
   mapping = {
-    ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-    ["<C-.>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<C-e>"] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ["<C-CR>"] = cmp.mapping.confirm({
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ["<C-s>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
+
     -- Tab mapping
     ['<Tab>'] = function(fallback)
       if cmp.visible() then
