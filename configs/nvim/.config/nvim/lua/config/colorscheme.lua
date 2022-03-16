f371d53fe63352fde48938728cb48b543c58186a
Sofi <sofi+git@mailbox.org>
2022-03-16 19:50:25 +0100

    Let MsgArea to have a dark background

--- configs/nvim/.config/nvim/lua/config/colorscheme.lua
+++ configs/nvim/.config/nvim/lua/config/colorscheme.lua
@@ -8,8 +8,12 @@ vim.g.tokyonight_sidebars = {
   "qf",
   "help",
 }
+vim.g.tokyonight_dark_sidebar = true
 
 -- HACK: workaround for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
 vim.wo.colorcolumn = "99999"
 
 require("tokyonight").colorscheme()
+
+-- Make command window and line-numbers have a dark background
+vim.cmd [[highlight MsgArea guibg=#16161e]]
