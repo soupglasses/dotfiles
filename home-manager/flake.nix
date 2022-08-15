cb5b9716117607dc37df7a379126f82b31c11f54
Sofi <sofi+git@mailbox.org>
2022-08-15 09:02:33 +0200

    flake.nix: update layout

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -1,19 +1,11 @@
 {
   description = "My personal dotfiles";
 
-  nixConfig.extra-experimental-features = "nix-command flakes";
-  nixConfig.extra-substituters = "https://nix-community.cachix.org https://imsofi.cachix.org";
-  nixConfig.extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= imsofi.cachix.org-1:KsqZ5nGoUfMHwzCGFnmTLMukGp7Emlrz/OE9Izq/nEM=";
-
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
     };
     imsofi-nur = {
       url = "github:imsofi/nur-pkgs";
@@ -29,33 +21,31 @@
   let
     system = "x86_64-linux";
     username = "sofi";
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
-          "geogebra"
+  in {
+    packages.${system} = import ./pkgs { pkgs = import nixpkgs { inherit system; }; };
+    overlays.default = (final: prev: import ./pkgs { pkgs = final; });
+
+    homeConfigurations.${username} = import "${home-manager}/modules" rec {
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
+            "geogebra"
+          ];
+        };
+        overlays = [
+          self.overlays.default
+          nur.overlay
         ];
       };
-
-      overlays = [
-        (final: prev: { home-manager = inputs.home-manager.packages.${prev.system}.home-manager; })
-        (final: prev: { ferium = inputs.imsofi-nur.packages.${prev.system}.ferium; })
-        (final: prev: import ./pkgs { pkgs = final; })
-        nur.overlay
-      ];
-    };
-    lib = pkgs.lib;
-  in {
-    homeConfigurations.${username} = import "${home-manager}/modules" {
-      inherit pkgs;
       check = true;
       configuration = {
         # Let us have a `inputs` argument inside of home-manager.
         _module.args.inputs = inputs;
 
         # WORKAROUND: https://github.com/nix-community/home-manager/pull/2720
-        _module.args.pkgs = lib.mkForce pkgs;
-        _module.args.pkgs_i686 = lib.mkForce { };
+        _module.args.pkgs = nixpkgs.lib.mkForce pkgs;
+        _module.args.pkgs_i686 = nixpkgs.lib.mkForce { };
 
         imports = [
           ./home.nix
@@ -66,9 +56,11 @@
       };
     };
 
-    devShell.${system} = pkgs.mkShell {
+    devShells.${system}.default = let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in pkgs.mkShell {
       buildInputs = [
-        pkgs.home-manager
+        inputs.home-manager.packages.${system}.home-manager
         pkgs.nixUnstable
       ];
     }; 
