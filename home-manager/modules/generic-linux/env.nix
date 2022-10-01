d23665e01de644af1b9506e28e0cc20b3ef6c6c8
Sofi <sofi+git@mailbox.org>
2022-10-01 15:58:17 +0200

    env: Add .local/bin to $PATH

--- home-manager/modules/generic-linux/env.nix
+++ home-manager/modules/generic-linux/env.nix
@@ -3,6 +3,8 @@ let
   initText = ''
     # WORKAROUND: https://github.com/nix-community/home-manager/issues/2751
     export EDITOR="$VISUAL"
+    # GENERIC_LINUX: Add user installed software
+    export PATH="${config.home.homeDirectory}/.local/bin:$PATH"
   '';
 in
 {
