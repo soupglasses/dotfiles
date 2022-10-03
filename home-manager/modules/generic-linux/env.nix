bddb35f6425b576a36c8103d4f63ee23c38d3b7b
Sofi <sofi+git@mailbox.org>
2022-10-03 09:23:30 +0200

    env: Use system libva/libgl/ld over nix versions

--- home-manager/modules/generic-linux/env.nix
+++ home-manager/modules/generic-linux/env.nix
@@ -3,8 +3,13 @@ let
   initText = ''
     # WORKAROUND: https://github.com/nix-community/home-manager/issues/2751
     export EDITOR="$VISUAL"
-    # GENERIC_LINUX: Add user installed software
+
+    # Add user installed software to PATH.
     export PATH="${config.home.homeDirectory}/.local/bin:$PATH"
+
+    # WORKAROUND: https://github.com/guibou/nixGL/issues/116
+    # Use system libva/libgl/ld by default over nix versions.
+    unset LIBVA_DRIVERS_PATH LIBGL_DRIVERS_PATH LD_LIBRARY_PATH
   '';
 in
 {
