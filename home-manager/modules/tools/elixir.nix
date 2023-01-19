6c43760ae705f804dd3d43a9e6a6841b10235ccf
Sofi <sofi+git@mailbox.org>
2023-01-19 02:36:31 +0100

    elixir editor aware

--- home-manager/modules/tools/elixir.nix
+++ home-manager/modules/tools/elixir.nix
@@ -2,6 +2,9 @@
   initText = ''
     # Elixir IEx Persistent History.
     export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
+
+    # Make Elixir IEx Editor aware.
+    export ELIXIR_EDITOR="nvim +__LINE__ __FILE__"
   '';
 in {
   home.file.".iex.exs".text = ''
