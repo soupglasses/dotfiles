c9194f18341aa56b03fe762da227212e40c542f4
Sofi <sofi+git@mailbox.org>
2023-01-21 17:33:30 +0100

    fix deadnix crash

--- nvim/pkgs/neovim/config/lua/my/lsp/null_ls.lua
+++ nvim/pkgs/neovim/config/lua/my/lsp/null_ls.lua
@@ -1,6 +1,6 @@
 local ok, null_ls = pcall(require, "null-ls")
 if not ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -11,14 +11,14 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.alejandra,
+  debug = false,
+  sources = {
+    formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.alejandra,
     code_actions.statix,
-    diagnostics.deadnix,
-		formatting.stylua,
-		null_ls.builtins.diagnostics.trail_space,
-	},
+    diagnostics.deadnix.with({ ignore_stderr = true }),
+    formatting.stylua,
+    null_ls.builtins.diagnostics.trail_space,
+  },
 })
