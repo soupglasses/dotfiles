bf78653bb0ef2c99e5c1637c22893c778dfc6851
Sofi <sofi+git@mailbox.org>
2023-01-22 22:34:47 +0100

    Update to use vim.keymap.set

--- nvim/pkgs/neovim/config/lua/util/init.lua
+++ nvim/pkgs/neovim/config/lua/util/init.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 M.map = function(mode, lhs, rhs, opts)
-  local options = { silent = true, noremap = true }
+  local options = { silent = true }
   if opts then
     options = vim.tbl_extend("force", options, opts)
   end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  vim.keymap.set(mode, lhs, rhs, options)
 end
 
 return M
