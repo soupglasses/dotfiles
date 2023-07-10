8c86f211c0973a5e8a0e691206a7392d5812286c
Sofi <sofi+git@mailbox.org>
2023-07-10 04:57:52 +0200

    default.nix: add packages to top level

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,10 +1,17 @@
-(import
-  (
-    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
-    fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
-    }
-  )
-  { src = ./.; }
-).defaultNix
+# Act like nixpkgs for update scripts.
+# Inspired by https://github.com/jtojnar/nixfiles/blob/master/default.nix
+{...}:
+let
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  flake-compat = fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  };
+  self = import flake-compat {
+    src = ./.;
+  };
+
+  packages = self.defaultNix.outputs.legacyPackages.${builtins.currentSystem};
+in
+packages # Prepend all packages to the top level, mimmicing nixpkgs.
+// self.defaultNix # Append flake-compat's arguments that follow flake formatting.
