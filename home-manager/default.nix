11a49de78af2ce9439279f33214c1c1e007e44c9
Sofi <sofi+git@mailbox.org>
2023-07-27 21:53:15 +0200

    default.nix: fix packages attribute

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -7,11 +7,9 @@ let
     url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
     sha256 = lock.nodes.flake-compat.locked.narHash;
   };
-  self = import flake-compat {
-    src = ./.;
-  };
-
-  packages = self.defaultNix.outputs.legacyPackages.${builtins.currentSystem};
+  self = import flake-compat { src = ./.; };
+  packages = self.defaultNix.outputs.packages.${builtins.currentSystem};
 in
+
 packages # Prepend all packages to the top level, mimmicing nixpkgs.
 // self.defaultNix # Append flake-compat's arguments that follow flake formatting.
