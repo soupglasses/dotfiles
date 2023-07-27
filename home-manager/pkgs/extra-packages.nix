57370a2d4acdf22cef60a2ca2aefa0ccf07b30b7
Sofi <sofi+git@mailbox.org>
2023-07-27 21:10:26 +0200

    ns-usbloader: init at 7.0

--- home-manager/pkgs/extra-packages.nix
+++ home-manager/pkgs/extra-packages.nix
@@ -3,4 +3,5 @@
   adw-gtk3 = pkgs.callPackage ./extras/adw-gtk3 { };
   nvim = import ./extras/neovim { inherit pkgs; };
   ps3iso-utils = pkgs.callPackage ./extras/ps3iso-utils { };
+  ns-usbloader = pkgs.callPackage ./extras/ns-usbloader { };
 }
